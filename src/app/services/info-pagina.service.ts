cd1f3aac81a211e01cae0f9a9c8b9036f836ae2f
louis.chen <imomi329@gmail.com>
2018-08-17 16:19:33 +0800

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,21 +9,35 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient ) {
 
     console.log('Servicio de infoPagina listo');
 
-    // Leer el archivo JSON
-    this.http.get('assets/data/data-pagina.json')
-      .subscribe( (resp: InfoPagina) => {
-
-        this.cargada = true;
-        this.info = resp;
-        console.log(resp);
-        // console.log( resp['twitter'] );
-
-      });
+    this.cargarInfo();
+    this.cargarEquipo();
 
   }
+
+  private cargarInfo() {
+    // Leer el archivo JSON
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe( (resp: InfoPagina) => {
+      this.cargada = true;
+      this.info = resp;
+      // console.log(resp);
+      // console.log( resp['twitter'] );
+    });
+  }
+
+  private cargarEquipo() {
+    // Leer el archivo JSON
+    this.http.get('https://angular-html-416cd.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+      this.equipo = resp;
+      // console.log(resp);
+    });
+  }
+
 }
